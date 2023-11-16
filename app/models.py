12d23ce04ccd69c46066602a924405fdf25b4219
Bryan Turrubiates <bryanturrubiates@outlook.com>
2023-11-16 14:32:04 -0700

    feat (DB) Added new columns on tables

--- app/models.py
+++ app/models.py
@@ -10,10 +10,12 @@ class Vehicles(db.Model):
     year = db.Column(db.Integer)
     motor = db.Column(db.String(50), nullable=False)
     traction = db.Column(db.String(50), nullable=False)
+    speedMax = db.Column(db.Integer, nullable=False)
+    power = db.Column(db.Integer, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
-    price = db.Column(db.Float, nullable=False)  # Corrección aquí
+    type = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
     urlImage = db.Column(db.Text, nullable=False)
-    # Agrega más campos según sea necesario
 
     def __init__(self, brand, model, year, motor, traction, stock, price, urlImage):
         self.brand = brand
@@ -24,7 +26,6 @@ class Vehicles(db.Model):
         self.stock = stock
         self.price = price
         self.urlImage = urlImage
-        # Puedes agregar más inicializaciones según sea necesario
 
 
 class Motorcycles(db.Model):
@@ -32,12 +33,22 @@ class Motorcycles(db.Model):
     brand = db.Column(db.String(50), nullable=False)
     model = db.Column(db.String(50), nullable=False)
     year = db.Column(db.Integer)
-    # Agrega más campos según sea necesario
+    cylinder = db.Column(db.Integer)
+    speedMax = db.Column(db.Integer)
+    stock = db.Column(db.Integer)
+    price = db.Column(db.Float, nullable=False)
+    urlImage = db.Column(db.Text, nullable=False)
 
-    def __init__(self, brand, model, year):
+    def __init__(self, brand, model, year, cylinder, speedMax, stock, price, urlImage):
         self.brand = brand
         self.model = model
         self.year = year
+        self.cylinder = cylinder
+        self.speedMax = speedMax
+        self.stock = stock
+        self.price = price
+        self.urlImage = urlImage
+
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,6 +58,7 @@ class Employee(db.Model):
     position = db.Column(db.String(50), nullable=False)
     department = db.Column(db.String(50), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
 
     def set_password(self, password):
         hash_object = hashlib.sha256()
@@ -55,13 +67,15 @@ class Employee(db.Model):
         hash_hex = hash_object.hexdigest()
         return hash_hex
     
-    def __init__(self, name, lastname ,email, position, department, password_hash):
+    def __init__(self, name, lastname, email, position, department, password_hash, is_active=True):
         self.name = name
         self.lastname = lastname
         self.email = email
         self.position = position
         self.department = department
         self.password_hash = self.set_password(password_hash)
+        self.is_active = is_active
+
         
 
 class Client(db.Model):
